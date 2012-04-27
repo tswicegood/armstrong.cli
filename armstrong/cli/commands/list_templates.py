b4aa46caab391afa913459a6f8481bc2d4d98992
Travis Swicegood <development@domain51.com>
2012-04-27 16:26:31 -0500

    Make sure that list_templates continues to work

--- armstrong/cli/commands/list_templates.py
+++ armstrong/cli/commands/list_templates.py
@@ -1,12 +1,12 @@
+from pkg_resources import iter_entry_points
 import os
+
+from .init import ENTRY_POINT
 
 
 def command():
-    # TODO: make this look for entry_points so third-party templates can be used
     # TODO: make this verify that there's a manifest.json present
     # TODO: make this display manifest.json's description along side template names
-    import armstrong.cli.templates
-    p = armstrong.cli.templates.__path__[0]
-    templates = [a for a in os.listdir(p) if os.path.isdir(os.path.join(p, a))]
     print "The following templates are available:"
-    print "\n    %s" % "\n    ".join(templates)
+    for entry_point in iter_entry_points(ENTRY_POINT):
+        print "    %s" % entry_point.name
